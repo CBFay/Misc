5d213b4fee7727bb98544b170ee125e4c8963908
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2018-02-23 15:45:21 -0500

    Create booleans.js

---
+++ Javascript/js_0005_variables/booleans.js
@@ -0,0 +1,11 @@
+// all of the following Boolean casts are false
+
+if ("") {alert("This is true!");}
+
+if (0) {alert("This is true!");}
+
+if (NaN) {alert("This is true!");}
+
+if (null) {alert("This is true!");}
+
+if (undefined) {alert("This is true!");}
